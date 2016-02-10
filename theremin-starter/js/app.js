7283516e00eb7f4ee111ed57a5700b77e6c50491
brenna <info@brennaobrien.com>
2016-02-09 20:46:44 -0500

    fix starter files

--- theremin-starter/js/app.js
+++ theremin-starter/js/app.js
@@ -1,6 +1,11 @@
 // we'll use these functions later
 // write your code above to get started
 
+function random(number1,number2) {
+  var randomNo = number1 + (Math.floor(Math.random() * (number2 - number1)) + 1);
+  return randomNo;
+}
+
 function updatePage(e) {
   CurX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
   CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
